2284b35d04fb7c5beb293b3cc4173223147a8676
student <amey.sab@gmail.com>
2022-06-02 16:55:37 +0000

    New files

---
+++ main.tf
@@ -0,0 +1,71 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+#Create security group with firewall rules
+resource "aws_security_group" "security_jenkins_grp" {
+  name        = var.security_group
+  description = "security group for jenkins"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = var.security_group
+  }
+
+provisioner "remote-exec" {
+ inline = [
+  "sudo apt update && upgrade",
+  "sudo apt install -y python3.8",
+  "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
+  "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
+  "sudo apt-get update",
+  "sudo apt-get install fontconfig openjdk-11-jre",
+  "sudo apt-get install -y net-tools",
+  "wget https://pkg.jenkins.io/debian-stable/binary/jenkins_2.332.3_all.deb",
+  "sudo dpkg -i jenkins_2.332.3_all.deb",
+  "sudo systemctl start jenkins",
+  "sudo systemctl enable jenkins"
+  ]
+}
+
+}
+
+resource "aws_instance" "terraform-ec2-instance" {
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  security_groups= [var.security_group]
+  tags= {
+    Name = var.tag_name
+  }
+}
+
+# Create Elastic IP address
+resource "aws_eip" "myElasticIP" {
+  vpc      = true
+  instance = aws_instance.terraform-ec2-instance.id
+tags= {
+    Name = "jenkins_elastic_ip"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+       description = "The AWS region to create things in." 
+       default     = "us-east-1" 
+}
+
+variable "key_name" { 
+    description = " SSH keys to connect to ec2 instance" 
+    default     =  "Devops_Terraform_Key" 
+}
+
+variable "instance_type" { 
+    description = "instance type for ec2" 
+    default     =  "t2.micro" 
+}
+
+variable "security_group" { 
+    description = "Name of security group" 
+    default     = "my-jenkins-security-group" 
+}
+
+variable "tag_name" { 
+    description = "Tag Name of for Ec2 instance" 
+    default     = "terraform-ec2-instance" 
+} 
+variable "ami_id" { 
+    description = "AMI for Ubuntu Ec2 instance" 
+    default     = "ami-0c4f7023847b90238"
+}
+
